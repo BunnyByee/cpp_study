f562b2c1d3a2e761a5e1c6396af5d9967f62bfcb
BunnyByee <khs.bunnybyee@gmail.com>
2026-01-06 17:53:49 +0900

    [new] c++ study start

---
+++ 260106-cpp start/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+	string s1 = "Good";
+	string s2 = "Morning";
+	// 연산자로 문자열을 합칠 수 있음 
+	string s3 = s1 + " " + s2 + "!\n";
+	cout << s3;
+	
+	return 0; 
+}
---
+++ 260106-cpp start/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	bool r1;
+	
+	r1 = 1 <= 2 && 3 >= 1;
+	
+	cout << r1;
+	
+	return 0;
+}
